dc10072a4e413a27bfd46346906833dc2ad3c1b1
Tate Fowble <meyus@Tates-MacBook-Pro.local>
2020-10-28 16:29:28 -0500

    updated randomNum Generator

--- scripts/randomGenerator.py
+++ scripts/randomGenerator.py
@@ -4,9 +4,22 @@ import random
 
 def randomGen(LowestN = 0,HighestN=10):
     print('Leave blank for default numbers to be used; 1 to 10')
-
+    
     LowestN = int(input('Min Number: '))
     HighestN = int(input('Max Number: '))
 
-    randomNum = random.randint(LowestN, HighestN)
-    print(''*5, randomNum)
+    if (LowestN and HighestN == 0):
+        randomNum = random.randint(0, 10)
+        print('')
+        print('')
+        print(''*5, randomNum)
+    else:
+        randomNum = random.randint(LowestN, HighestN)
+        print('')
+        print('')
+        print(''*5, randomNum)
+        
+    # except (LowestN or HighestN == 0 or str):
+    #     print(LowestN)
+    #     print('Testing')
+
